27d63ca9784ad9af45d8943c04edfb48cf1268a1
Your Name <thoai94no1@gmail.com>
2023-05-05 08:34:55 +0700

    add file SystemTime.java

---
+++ SystemTime.java
@@ -0,0 +1,12 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+
+import java.util.Date;
+
+public class SystemTime {
+    public static void main(String[] args) {
+        Date now = new Date(); // Lấy ra thời gian hiện tại
+
+        System.out.println("Now is: " + now);
+    }
+}
